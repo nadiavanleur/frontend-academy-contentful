52c438c0e55d67e130ef8e414d8d64bbc0cef738
Nadia van Leur <nadiavl@hotmail.com>
2019-06-16 13:04:53 +0200

    Progress bar working

--- src/components/progress.js
+++ src/components/progress.js
@@ -1,16 +1,24 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Progress extends Component {
   render() {
     const { progress, elementClass } = this.props
 
     return (
-      <progress
-        className={`${elementClass} progress`}
-        value={progress !== undefined && progress !== null ? progress : 0}
-        max="100"
-      />
+      <React.Fragment>
+        <progress
+          className={`${elementClass} progress sr-only`}
+          value={progress !== undefined && progress !== null ? progress : 0}
+          max="100"
+        />
+        <div className={`${elementClass} progress`}>
+          <span
+            class="progress__inner"
+            style={{ transform: `translateX(` + progress + `%)` }}
+          />
+        </div>
+      </React.Fragment>
     )
   }
 }
